7e3dea7b66ff0e1369b33fff6997d4ea4efdc493
Hooman <hesamyan@gmail.com>
2021-02-04 01:22:53 -0500

    Update tabular.jsx

--- components/tabular.jsx
+++ components/tabular.jsx
@@ -142,8 +142,8 @@ class Tabular extends Component {
                       return Tabular.sortHandler(y[sortKey], x[sortKey]);
                     }
                   })
-                  .map((x) => (
-                    <tr key={Math.random()}>
+                  .map((x, i) => (
+                    <tr key={i}>
                       <td>{x.store}</td>
                       <td>{x.name}</td>
                       <td>{x.initialPrice}</td>
